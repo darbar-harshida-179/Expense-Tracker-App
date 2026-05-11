c8802bc98ae34707fb2a55202f303cdca486db58
darbar-harshida-179 <darbarharshida179@gmail.com>
2026-05-11 19:18:07 +0530

    created oAuth setup

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,10 +2,27 @@
 
 import express from 'express';
 import { login, register } from "../controllers/authController.js";
+import passport from 'passport';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+
+router.get('/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }
+    ));
+
+router.get('/google/callback',
+    passport.authenticate('google', {
+        session: false,
+        failureRedirect: '/login'
+    }),
+    (req, res) => {
+        res.send('Google Login Successfull!');
+    }
+)
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,28 @@
 
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import passport from 'passport';
+
+
+import './config/passport.js';
 
 import expenseRoutes from './routes/expenseRoutes.js';
 import authRoutes from './routes/authRoutes.js';
-
-dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use(passport.initialize());
+
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB connected successfully!"))
-.catch((err) => console.log("MongoDB Error:", err));
+    .then(() => console.log("MongoDB connected successfully!"))
+    .catch((err) => console.log("MongoDB Error:", err));
 
 app.use('/api/auth', authRoutes);
 app.use('/api', expenseRoutes);
@@ -28,4 +34,4 @@ app.get('/', (req, res) => {
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
-}); 
+});
